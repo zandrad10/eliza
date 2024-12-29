c6045757577e6c04b345bc76eee6464660286355
jin <32600939+madjin@users.noreply.github.com>
2024-12-28 22:15:38 -0500

    docs: Add JSDoc documentation to payments.ts

--- packages/plugin-coinbase/advanced-sdk-ts/src/rest/payments.ts
+++ packages/plugin-coinbase/advanced-sdk-ts/src/rest/payments.ts
@@ -9,6 +9,12 @@ import { method } from './types/request-types';
 
 // [GET] List Payment Methods
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_getpaymentmethods
+/**
+ * Fetches the list of payment methods available.
+ * 
+ * @this RESTBase
+ * @returns {Promise<ListPaymentMethodsResponse>} - A promise that resolves with the list of payment methods.
+ */
 export function listPaymentMethods(
   this: RESTBase
 ): Promise<ListPaymentMethodsResponse> {
@@ -21,6 +27,12 @@ export function listPaymentMethods(
 
 // [GET] Get Payment Method
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_getpaymentmethod
+/**
+ * Retrieves a payment method by its ID.
+ * 
+ * @param {GetPaymentMethodRequest} - The request object containing the paymentMethodId.
+ * @returns {Promise<GetPaymentMethodResponse>} - A promise that resolves to the retrieved payment method.
+ */
 export function getPaymentMethod(
   this: RESTBase,
   { paymentMethodId }: GetPaymentMethodRequest
