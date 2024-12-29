97efb5896724b2e05a98b526f5f4de5da4f0bc28
jin <32600939+madjin@users.noreply.github.com>
2024-12-28 22:15:47 -0500

    docs: Add JSDoc documentation to request-types.ts

--- packages/plugin-coinbase/advanced-sdk-ts/src/rest/types/request-types.ts
+++ packages/plugin-coinbase/advanced-sdk-ts/src/rest/types/request-types.ts
@@ -1,3 +1,11 @@
+/**
+ * Enum representing HTTP methods.
+ * @enum {string}
+ * @property {string} GET - GET method
+ * @property {string} POST - POST method
+ * @property {string} PUT - PUT method
+ * @property {string} DELETE - DELETE method
+ */
 export enum method {
   GET = 'GET',
   POST = 'POST',
@@ -5,6 +13,15 @@ export enum method {
   DELETE = 'DELETE',
 }
 
+/**
+ * Interface representing options for making a request.
+ * @typedef {Object} RequestOptions
+ * @property {string} method - The HTTP method for the request.
+ * @property {string} endpoint - The API endpoint URL.
+ * @property {Object.<string, any>} [queryParams] - Optional query parameters for the request.
+ * @property {Object.<string, any>} [bodyParams] - Optional body parameters for the request.
+ * @property {boolean} isPublic - Flag indicating if the request is for a public endpoint.
+ */
 export interface RequestOptions {
   method: method;
   endpoint: string;
