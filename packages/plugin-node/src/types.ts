9a1f9addef9e239c3e95f04f3929e404350aa166
jin <32600939+madjin@users.noreply.github.com>
2024-12-28 22:17:01 -0500

    docs: Add JSDoc documentation to types.ts

--- packages/plugin-node/src/types.ts
+++ packages/plugin-node/src/types.ts
@@ -4,8 +4,16 @@ export const FileLocationResultSchema = z.object({
     fileLocation: z.string().min(1),
 });
 
+/**
+ * Represents the result type inferred from the schema for FileLocationResult.
+ */
 export type FileLocationResult = z.infer<typeof FileLocationResultSchema>;
 
+/**
+ * Check if the input object is an instance of FileLocationResult.
+ * @param {unknown} obj - The input object to be checked.
+ * @returns {boolean} Whether the input object is an instance of FileLocationResult.
+ */
 export function isFileLocationResult(obj: unknown): obj is FileLocationResult {
     return FileLocationResultSchema.safeParse(obj).success;
 }
