120f24578f0a574e824bfe2e90d4456dfb113eae
jin <32600939+madjin@users.noreply.github.com>
2024-12-28 22:15:38 -0500

    docs: Add JSDoc documentation to orders.ts

--- packages/plugin-coinbase/advanced-sdk-ts/src/rest/orders.ts
+++ packages/plugin-coinbase/advanced-sdk-ts/src/rest/orders.ts
@@ -24,6 +24,12 @@ import { method } from './types/request-types';
 
 // [POST] Create Order
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_postorder
+/**
+ * Creates a new order by sending a POST request to the API endpoint with the provided request parameters.
+ * 
+ * @param {CreateOrderRequest} requestParams - The parameters needed to create the order.
+ * @returns {Promise<CreateOrderResponse>} A Promise that resolves to the response from the API with the created order.
+ */
 export function createOrder(
   this: RESTBase,
   requestParams: CreateOrderRequest
@@ -38,6 +44,12 @@ export function createOrder(
 
 // [POST] Cancel Orders
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_cancelorders
+/**
+ * Cancels multiple orders.
+ * 
+ * @param {CancelOrdersRequest} requestParams - The parameters for cancelling the orders.
+ * @returns {Promise<CancelOrdersResponse>} - A promise that resolves with the response of the cancellation operation.
+ */
 export function cancelOrders(
   this: RESTBase,
   requestParams: CancelOrdersRequest
@@ -52,6 +64,12 @@ export function cancelOrders(
 
 // [POST] Edit Order
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_editorder
+/**
+ * Edit an order.
+ * 
+ * @param {EditOrderRequest} requestParams - The request parameters for editing the order.
+ * @returns {Promise<EditOrderResponse>} - A promise that resolves with the response from editing the order.
+ */
 export function editOrder(
   this: RESTBase,
   requestParams: EditOrderRequest
@@ -66,6 +84,12 @@ export function editOrder(
 
 // [POST] Edit Order Preview
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_previeweditorder
+/**
+ * Function to edit an order preview.
+ * 
+ * @param {EditOrderPreviewRequest} requestParams - The request parameters for editing the order preview.
+ * @returns {Promise<EditOrderPreviewResponse>} A Promise that resolves with the response of editing the order preview.
+ */
 export function editOrderPreview(
   this: RESTBase,
   requestParams: EditOrderPreviewRequest
@@ -80,6 +104,11 @@ export function editOrderPreview(
 
 // [GET] List Orders
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_gethistoricalorders
+/**
+ * Function to list historical orders in batch.
+ * @param {ListOrdersRequest} requestParams - The request parameters for listing orders.
+ * @returns {Promise<ListOrdersResponse>} A promise that resolves with the list of historical orders response.
+ */
 export function listOrders(
   this: RESTBase,
   requestParams: ListOrdersRequest
@@ -94,6 +123,12 @@ export function listOrders(
 
 // [GET] List Fills
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_getfills
+/**
+ * Retrieves a list of historical fills for orders.
+ * 
+ * @param {ListFillsRequest} requestParams - The parameters to use when fetching the list of fills
+ * @returns {Promise<ListFillsResponse>} - A promise that resolves with the list of fills response
+ */
 export function listFills(
   this: RESTBase,
   requestParams: ListFillsRequest
@@ -108,6 +143,12 @@ export function listFills(
 
 // [GET] Get Order
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_gethistoricalorder
+/**
+ * Get details of a specific order.
+ * @param {Object} options - The options to request the order.
+ * @param {string} options.orderId - The ID of the order to retrieve.
+ * @return {Promise<GetOrderResponse>} - The response containing the details of the order.
+ */
 export function getOrder(
   this: RESTBase,
   { orderId }: GetOrderRequest
@@ -121,6 +162,12 @@ export function getOrder(
 
 // [POST] Preview Order
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_previeworder
+/**
+ * Preview an order before placing it.
+ *
+ * @param {PreviewOrderRequest} requestParams - The parameters for the order preview.
+ * @returns {Promise<PreviewOrderResponse>} A promise that resolves with the preview order response.
+ */
 export function previewOrder(
   this: RESTBase,
   requestParams: PreviewOrderRequest
@@ -135,6 +182,11 @@ export function previewOrder(
 
 // [POST] Close Position
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_closeposition
+/**
+ * Close a position in the user's account.
+ * @param {ClosePositionRequest} requestParams - The request parameters for closing the position.
+ * @returns {Promise<ClosePositionResponse>} A promise that resolves with the response from closing the position.
+ */
 export function closePosition(
   this: RESTBase,
   requestParams: ClosePositionRequest
