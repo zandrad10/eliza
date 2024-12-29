1f8f61eb95b8e9b09ae444488fca058c94e5d6de
jin <32600939+madjin@users.noreply.github.com>
2024-12-28 22:15:42 -0500

    docs: Add JSDoc documentation to dataAPI-types.ts

--- packages/plugin-coinbase/advanced-sdk-ts/src/rest/types/dataAPI-types.ts
+++ packages/plugin-coinbase/advanced-sdk-ts/src/rest/types/dataAPI-types.ts
@@ -1,6 +1,15 @@
 import { PortfolioType } from './common-types';
 
 // Get API Key Permissions
+/**
+ * Response object for getting API key permissions.
+ * @typedef {object} GetAPIKeyPermissionsResponse
+ * @property {boolean} [can_view] - Indicates if user has permission to view
+ * @property {boolean} [can_trade] - Indicates if user has permission to trade
+ * @property {boolean} [can_transfer] - Indicates if user has permission to transfer
+ * @property {string} [portfolio_uuid] - UUID of portfolio associated with permissions
+ * @property {PortfolioType} [portfolio_type] - Type of portfolio associated with permissions
+ */
 export type GetAPIKeyPermissionsResponse = {
   can_view?: boolean;
   can_trade?: boolean;
