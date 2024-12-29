a1fff3ebcac9064c4a4a86d6cddafacef60b5540
jin <32600939+madjin@users.noreply.github.com>
2024-12-28 22:14:30 -0500

    docs: Add JSDoc documentation to slack-types.ts

--- packages/client-slack/src/types/slack-types.ts
+++ packages/client-slack/src/types/slack-types.ts
@@ -1,6 +1,17 @@
 import { WebClient } from "@slack/web-api";
 import { Service, ServiceType } from "@elizaos/core";
 
+/**
+ * Interface representing the configuration for a Slack app.
+ * @typedef {Object} SlackConfig
+ * @property {string} appId - The ID of the Slack app.
+ * @property {string} clientId - The client ID of the Slack app.
+ * @property {string} clientSecret - The client secret of the Slack app.
+ * @property {string} signingSecret - The signing secret of the Slack app.
+ * @property {string} verificationToken - The verification token of the Slack app.
+ * @property {string} botToken - The bot token of the Slack app.
+ * @property {string} botId - The ID of the Slack bot associated with the app.
+ */
 export interface SlackConfig {
     appId: string;
     clientId: string;
@@ -11,11 +22,25 @@ export interface SlackConfig {
     botId: string;
 }
 
+/**
+ * Interface representing the context for the Slack client.
+ * @property {any} client - The Slack client.
+ * @property {SlackConfig} config - The configuration for the Slack client.
+ */
 export interface SlackClientContext {
     client: any;
     config: SlackConfig;
 }
 
+/**
+ * Interface representing a Slack message.
+ *
+ * @property {string} text - The text content of the message.
+ * @property {string} userId - The user ID of the sender.
+ * @property {string} channelId - The channel ID where the message was sent.
+ * @property {string} [threadTs] - Optional timestamp for threading messages.
+ * @property {Array<{type: string, url: string, title: string, size: number}>} [attachments] - Optional array of message attachments.
+ */
 export interface SlackMessage {
     text: string;
     userId: string;
@@ -34,6 +59,11 @@ export interface SlackMessage {
 export const SLACK_SERVICE_TYPE = ServiceType.TEXT_GENERATION;
 
 // Interface extending core Service
+/**
+ * Interface for Slack service that extends Service
+ * @interface
+ * @property {WebClient} client - The web client used for communication with Slack API
+ */
 export interface ISlackService extends Service {
     client: WebClient;
 }
