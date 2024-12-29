60924b6f85b6da716ad0c59c5093ab1746371376
jin <32600939+madjin@users.noreply.github.com>
2024-12-28 22:15:34 -0500

    docs: Add JSDoc documentation to converts.ts

--- packages/plugin-coinbase/advanced-sdk-ts/src/rest/converts.ts
+++ packages/plugin-coinbase/advanced-sdk-ts/src/rest/converts.ts
@@ -12,6 +12,11 @@ import { method } from './types/request-types';
 
 // [POST] Create Convert Quote
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_createconvertquote
+/**
+ * Creates a new convert quote.
+ * @param {CreateConvertQuoteRequest} requestParams - The parameters for creating the convert quote.
+ * @returns {Promise<CreateConvertQuoteResponse>} A promise that resolves with the response of creating the convert quote.
+ */
 export function createConvertQuote(
   this: RESTBase,
   requestParams: CreateConvertQuoteRequest
@@ -26,6 +31,12 @@ export function createConvertQuote(
 
 // [GET] Get Convert Trade
 // Official Documentation: https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_getconverttrade
+/**
+ * Retrieves information about a specific converted trade.
+ * 
+ * @param {GetConvertTradeRequest} options - The options for the request, including the trade ID to retrieve information for.
+ * @returns {Promise<GetConvertTradeResponse>} A Promise that resolves with the response data of the converted trade.
+ */
 export function getConvertTrade(
   this: RESTBase,
   { tradeId, ...requestParams }: GetConvertTradeRequest
@@ -40,6 +51,13 @@ export function getConvertTrade(
 
 // [POST] Commit Connvert Trade
 // https://docs.cdp.coinbase.com/advanced-trade/reference/retailbrokerageapi_commitconverttrade
+/**
+ * Converts trade by making a POST request to the API endpoint.
+ * @param {CommitConvertTradeRequest} options - The options for converting the trade.
+ * @param {string} options.tradeId - The ID of the trade to convert.
+ * @param {any} options.requestParams - Other request parameters for converting the trade.
+ * @returns {Promise<CommitConvertTradeResponse>} A Promise that resolves with the response data of the converted trade.
+ */
 export function commitConvertTrade(
   this: RESTBase,
   { tradeId, ...requestParams }: CommitConvertTradeRequest
