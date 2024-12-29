8773e38abab7e4ce1367f192429ef7348b0f4961
jin <32600939+madjin@users.noreply.github.com>
2024-12-28 22:15:45 -0500

    docs: Add JSDoc documentation to payments-types.ts

--- packages/plugin-coinbase/advanced-sdk-ts/src/rest/types/payments-types.ts
+++ packages/plugin-coinbase/advanced-sdk-ts/src/rest/types/payments-types.ts
@@ -1,16 +1,31 @@
 import { PaymentMethod } from './common-types';
 
 // List Payment Methods
+/**
+ * Response object for listing payment methods.
+ * @typedef {Object} ListPaymentMethodsResponse
+ * @property {PaymentMethod} paymentMethods - The list of payment methods available.
+ */
 export type ListPaymentMethodsResponse = {
   paymentMethods?: PaymentMethod;
 };
 
 // Get Payment Method
+/**
+ * Defines the shape of the request object for retrieving a specific payment method.
+ * @typedef {Object} GetPaymentMethodRequest
+ * @property {string} paymentMethodId - The unique identifier of the payment method to retrieve.
+ */
 export type GetPaymentMethodRequest = {
   // Path Params
   paymentMethodId: string;
 };
 
+/**
+ * Response object for getting a payment method.
+ * @typedef {Object} GetPaymentMethodResponse
+ * @property {PaymentMethod} [paymentMethod] - The payment method object.
+ */
 export type GetPaymentMethodResponse = {
   paymentMethod?: PaymentMethod;
 };
