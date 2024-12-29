34f4938c97e9df66302a9586faddc147cfa39277
jin <32600939+madjin@users.noreply.github.com>
2024-12-28 22:17:27 -0500

    docs: Add JSDoc documentation to uploadJSONToIPFS.ts

--- packages/plugin-story/src/functions/uploadJSONToIPFS.ts
+++ packages/plugin-story/src/functions/uploadJSONToIPFS.ts
@@ -1,5 +1,11 @@
 import PinataClient from "@pinata/sdk";
 
+/**
+ * Uploads a JSON object to IPFS using the Pinata client.
+ * @param {PinataClient} pinata - The Pinata client used to interact with Pinata's API.
+ * @param {*} jsonMetadata - The JSON object to be uploaded to IPFS.
+ * @returns {Promise<string>} The IPFS hash of the uploaded JSON object.
+ */
 export async function uploadJSONToIPFS(
     pinata: PinataClient,
     jsonMetadata: any
