3710dbe6dba2d00b9e342c9597f809f7f813d231
jin <32600939+madjin@users.noreply.github.com>
2024-12-28 22:16:45 -0500

    docs: Add JSDoc documentation to amount.ts

--- packages/plugin-multiversx/src/utils/amount.ts
+++ packages/plugin-multiversx/src/utils/amount.ts
@@ -2,6 +2,11 @@ import BigNumber from "bignumber.js";
 
 BigNumber.config({ ROUNDING_MODE: BigNumber.ROUND_FLOOR });
 
+/**
+ * Defines the PayloadType object with the following properties:
+ * - amount: a string representing the amount
+ * - decimals: a number representing the number of decimals
+ */
 type PayloadType = {
     amount: string;
     decimals: number;
